cec95426e24e03ff194c3e01757df9600070d1ea
felipectr05 <felipectr7@gmail.com>
2023-08-23 21:16:12 -0300

    React

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,20 @@
+import styled, { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    border: 0;
+    box-sizing: border-box;
+  }
+`
+export const colors = {
+  primary: "#2b782b",
+  secondary: "#2d6147",
+  secondaryLight: "#1eeb74",
+  third: "#4a6e35",
+  thirdLight: "#417a4d",
+  black: "#000",
+  white: "#fff"
+}
